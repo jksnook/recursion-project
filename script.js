e6c80ff4685acefae11aebed0f15d903a6d88e55
John <134107082+jksnook@users.noreply.github.com>
2023-10-03 16:53:59 -0700

    write merge sort function

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function fibsRec (n) {
   let shorterFibs = fibsRec(n - 1);
   return shorterFibs.concat(shorterFibs[n - 2] + shorterFibs[n - 3]);
 }
+
+function mergeSort(a) {
+  if (a.length === 1) return a;
+
+  function merge(b, c) {
+    const sorted = [];
+    while (b.length > 0 || c.length > 0) {
+      if (b[0] > c[0] || b.length === 0) {
+        sorted.push(c.shift());
+      } else {
+        sorted.push(b.shift());
+      }
+    }
+    return sorted;
+  }
+
+  let firstHalf = a.slice(0, (a.length - a.length % 2) / 2);
+  let secondHalf = a.slice((a.length - a.length % 2) / 2);
+
+  return merge(mergeSort(firstHalf), mergeSort(secondHalf));
+}
